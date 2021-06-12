e573b41271850d223e12fb23e01db6af52ae36f2
EL2020 <el2020202020@gmail.com>
2021-06-12 17:36:16 -0400

    removed unneeded dictionary

--- CCUtils.py
+++ CCUtils.py
@@ -18,24 +18,6 @@ class Character:
             case 'hlin': return 'main.guild-leader'
             case _: return charName
 
-
-    {
-        'lea': 'main.lea',
-        'emilie': 'main.emilie',
-        'c\'tron': 'main.glasses',
-        'apollo': 'antagonists.fancyguy',
-        'joern': 'antagonists.sidekick',
-        'shizuka': 'main.shizuka',
-        'lukas': 'main.schneider',
-        'schneider': 'main.schneider',
-        'luke': 'main.luke',
-        'sergey': 'main.sergey',
-        'sergey (avatar)': 'main.sergey-av',
-        'beowulf': 'main.grumpy',
-        'buggy': 'main.buggy',
-        'hlin': 'main.guild-leader'
-    }
-
     def __init__(self, name: str, expression: str, internalName: str = None) -> None:
         self.name: str = name
         self.expression: str = expression
